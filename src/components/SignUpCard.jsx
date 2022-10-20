2e60bf2c9c5f43b67d7a46bdedc95affb6f9a59e
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2022-10-20 11:55:38 +0300

    basic app logic done

--- src/components/SignUpCard.jsx
+++ src/components/SignUpCard.jsx
@@ -1,53 +1,79 @@
 import React, { useState } from "react";
-import { useInput } from "../hooks/useInput";
+
+import ErrorIcon from "./assets/img/icon-error.svg";
 
 import { isEmpty } from "../utils/isEmpty";
 
 import styles from "./assets/css/SignUpCard.module.css";
 
 function SignUpCard() {
-  const signUpInputs = {
-    firstName: useInput(""),
-    lastName: useInput(""),
-    email: useInput(""),
-    password: useInput(""),
-  };
-
-  const [fnameMessage, setFNameMessage] = useState("");
-  const [lnameMessage, setLNameMessage] = useState("");
-  const [emailMessage, setEmailMessage] = useState("");
-  const [passwordMessage, setPasswordMessage] = useState("");
+  const [firstName, setFirstName] = useState({
+    value: "",
+    placeholder: "First Name",
+    message: "First Name cannot be empty",
+    isCorrect: true,
+  });
+  const [lastName, setLastName] = useState({
+    value: "",
+    placeholder: "Last Name",
+    message: "Last Name cannot be empty",
+    isCorrect: true,
+  });
+  const [email, setEmail] = useState({
+    value: "",
+    placeholder: "Email Address",
+    message: "Looks like not a email address",
+    isCorrect: true,
+  });
+  const [password, setPassword] = useState({
+    value: "",
+    placeholder: "Email Address",
+    message: "Password cannot be empty",
+    isCorrect: true,
+  });
 
   function checkFirstName() {
-    if (isEmpty(signUpInputs.firstName.value)) {
-      setFNameMessage("First Name cannot be empty");
+    if (isEmpty(firstName.value)) {
+      setFirstName({ ...firstName, isCorrect: false });
     } else {
-      setFNameMessage("");
+      setFirstName({ ...firstName, isCorrect: true });
     }
   }
 
   function checkLastName() {
-    if (isEmpty(signUpInputs.lastName.value)) {
-      setLNameMessage("Last name cannot be empty");
+    if (isEmpty(lastName.value)) {
+      setLastName({ ...lastName, isCorrect: false });
     } else {
-      setLNameMessage("");
+      setLastName({ ...lastName, isCorrect: true });
     }
   }
 
   function checkEmail() {
-    if (isEmpty(signUpInputs.email.value)) {
-      setEmailMessage("Looks like this is not an email");
+    if (isEmpty(email.value)) {
+      setEmail({ ...email, isCorrect: false });
     } else {
-      setEmailMessage("");
+      setEmail({ ...email, isCorrect: true });
     }
   }
 
   function checkPassword() {
-    if (isEmpty(signUpInputs.password.value)) {
-      setPasswordMessage("Password cannot be empty");
+    if (isEmpty(password.value)) {
+      setPassword({ ...password, isCorrect: false });
     } else {
-      setPasswordMessage("");
+      setPassword({ ...password, isCorrect: true });
     }
+  }
+
+  function isInputFieldCorrect(iStyles, eStyles) {
+    if (
+      firstName.isCorrect === true &&
+      lastName.isCorrect === true &&
+      email.isCorrect === true &&
+      password.isCorrect === true
+    ) {
+      return iStyles;
+    }
+    return eStyles;
   }
 
   function sendSignUpForm(e) {
@@ -65,40 +91,68 @@ function SignUpCard() {
           <strong>Try it free 7 days</strong> then $20/mo.thereafter
         </p>
       </div>
-      <form className={styles["sign-up-card"]} onSubmit={sendSignUpForm}>
+      <form
+        className={`${styles["sign-up-card"]} ${isInputFieldCorrect(
+          styles["card-right-padding-correct"],
+          styles["card-right-padding-incorrect"]
+        )}`}
+        onSubmit={sendSignUpForm}
+      >
         <div className={`${styles["sign-up-card-item"]}`}>
-          <input
-            type="text"
-            placeholder="First name"
-            {...signUpInputs.firstName}
-          />
-          <label>{fnameMessage}</label>
+          <div className={`${styles["card-item-input"]}`}>
+            <input
+              type="text"
+              placeholder="First name"
+              value={firstName.value}
+              onChange={(e) =>
+                setFirstName({ ...firstName, value: e.target.value })
+              }
+            />
+            {firstName.isCorrect ? null : <img src={ErrorIcon} alt="" />}
+          </div>
+          <label>{firstName.isCorrect ? null : firstName.message}</label>
         </div>
         <div className={`${styles["sign-up-card-item"]}`}>
-          <input
-            type="text"
-            placeholder="Last name"
-            {...signUpInputs.lastName}
-          />
-          <label>{lnameMessage}</label>
+          <div className={`${styles["card-item-input"]}`}>
+            <input
+              type="text"
+              placeholder="Last name"
+              value={lastName.value}
+              onChange={(e) =>
+                setLastName({ ...lastName, value: e.target.value })
+              }
+            />
+            {lastName.isCorrect ? null : <img src={ErrorIcon} alt="" />}
+          </div>
+          <label>{lastName.isCorrect ? null : lastName.message}</label>
         </div>
         <div className={`${styles["sign-up-card-item"]}`}>
-          <input
-            type="email"
-            placeholder="Email address"
-            {...signUpInputs.email}
-          />
-          <label>{emailMessage}</label>
+          <div className={`${styles["card-item-input"]}`}>
+            <input
+              type="email"
+              placeholder="Email address"
+              value={email.value}
+              onChange={(e) => setEmail({ ...email, value: e.target.value })}
+            />
+            {email.isCorrect ? null : <img src={ErrorIcon} alt="" />}
+          </div>
+          <label>{email.isCorrect ? null : email.message}</label>
         </div>
         <div className={`${styles["sign-up-card-item"]}`}>
-          <input
-            type="password"
-            placeholder="Password"
-            {...signUpInputs.password}
-          />
-          <label>{passwordMessage}</label>
+          <div className={`${styles["card-item-input"]}`}>
+            <input
+              type="password"
+              placeholder="Password"
+              value={password.value}
+              onChange={(e) =>
+                setPassword({ ...password, value: e.target.value })
+              }
+            />
+            {password.isCorrect ? null : <img src={ErrorIcon} alt="" />}
+          </div>
+          <label>{password.isCorrect ? null : password.message}</label>
         </div>
-        <button>Claim youre free trial</button>
+        <button className={isInputFieldCorrect("", styles["w-95"])}>Claim youre free trial</button>
         <p>
           By clicking the button, you are agreeing to out
           <span> Terms and services</span>
